af49b3adc4bd88531ecf3c220e7df6dfe7df7108
David Wilhelm <dewilhelm@gmail.com>
2015-05-12 00:10:27 -0400

    customize layout

--- keyboard/ergodox/keymap.c
+++ keyboard/ergodox/keymap.c
@@ -90,29 +90,30 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KEYMAP(  // layer 0 : default
         // left hand
-        EQL, 1,   2,   3,   4,   5,   ESC,
-        BSLS,Q,   W,   E,   R,   T,   FN2,
-        TAB, A,   S,   D,   F,   G,
-        LSFT,Z,   X,   C,   V,   B,   FN1,
-        LGUI,GRV, BSLS,LEFT,RGHT,
-                                      LCTL,LALT,
-                                           HOME,
-                                 BSPC,DEL, END,
+        ESC, F1,   F2,   F3,   F4,  F5,   F6,
+        TAB, QUOT, COMM, DOT,  P,   Y,   SPC,
+        LCTL, A,   O,   E,   U,   I,
+        FN2, SCLN,   Q,   J,   K,   X,   SPC,
+        LGUI,SPC, SPC, SPC, LALT,
+                                      VOLD, VOLU,
+                                           MUTE,
+                                 LSFT, FN1, FN3,
+
         // right hand
-             FN3, 6,   7,   8,   9,   0,   MINS,
-             LBRC,Y,   U,   I,   O,   P,   RBRC,
-                  H,   J,   K,   L,   SCLN,QUOT,
-             FN1, N,   M,   COMM,DOT, SLSH,RSFT,
-                       LEFT,DOWN,UP,  RGHT,RGUI,
-        RALT,RCTL,
-        PGUP,
-        PGDN,ENT, SPC
+             F7, F8,   F9,   F10,   F11,   F12,   ESC,
+             ENT, F,   G,   C,   R,   L,   SLSH,
+                  D,   H,   T,   N,   S, MINUS,
+             ENT, B,   M,   W, V, Z, RSFT,
+                       BSPACE, DELETE, LEFT, RGHT,RGUI,
+        MPRV,MNXT,
+        MPLY,
+        DEL,ENT, SPC
     ),
 
-    KEYMAP(  // layer 1 : function and symbol keys
+    KEYMAP(  // layer 1 : symbol keys
         // left hand
-        TRNS,F1,  F2,  F3,  F4,  F5,  F11,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,FN4,
+        TRNS,TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,TRNS,TRNS,TRNS,
@@ -120,19 +121,19 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                            TRNS,
                                  TRNS,TRNS,TRNS,
         // right hand
-             F12, F6,  F7,  F8,  F9,  F10, TRNS,
-             TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-                  TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-             TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+             TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, TRNS,
+             TRNS, FN4, FN5,FN6,FN7,FN8,BSLS,
+                  FN9,FN10,FN11, EQL,SCLN,FN12,
+             TRNS,GRV,LBRC,RBRC,FN13,FN14,TRNS,
                        TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,
         TRNS,
         TRNS,TRNS,TRNS
     ),
 
-    KEYMAP(  // layer 2 : keyboard functions
+    KEYMAP(  // layer 2 : number pad 
         // left hand
-        FN0, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
@@ -141,17 +142,17 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                            TRNS,
                                  TRNS,TRNS,TRNS,
         // right hand
-             FN4, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-             TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-                  TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-             TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+             TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+             TRNS,FN15,1,2,3,FN16,TRNS,
+                  PPLS,4,5,6,0,FN17,
+             TRNS,FN18,7,8,9,FN19,TRNS,
                        TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,
         TRNS,
         TRNS,TRNS,TRNS
     ),
 
-    KEYMAP(  // layer 3: numpad
+    KEYMAP(  // layer 3: navigation
         // left hand
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
@@ -162,11 +163,11 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                            TRNS,
                                  TRNS,TRNS,TRNS,
         // right hand
-             TRNS,NLCK,PSLS,PAST,PAST,PMNS,BSPC,
-             TRNS,NO,  P7,  P8,  P9,  PMNS,BSPC,
-                  NO,  P4,  P5,  P6,  PPLS,PENT,
-             TRNS,NO,  P1,  P2,  P3,  PPLS,PENT,
-                       P0,  PDOT,SLSH,PENT,PENT,
+             TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+             TRNS,TRNS,  TRNS,  TRNS,  TRNS,  TRNS, TRNS,
+                  TRNS,  LEFT,  DOWN,  UP,  RIGHT, TRNS,
+             TRNS,TRNS,  HOME,  PGDOWN, PGUP , END, TRNS,
+                       TRNS,  TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,
         TRNS,
         TRNS,TRNS,TRNS
@@ -185,9 +186,25 @@ enum function_id {
 static const uint16_t PROGMEM fn_actions[] = {
     ACTION_FUNCTION(TEENSY_KEY),                    // FN0 - Teensy key
     ACTION_LAYER_MOMENTARY(1),                      // FN1 - switch to Layer1
-    ACTION_LAYER_SET(2, ON_PRESS),                  // FN2 - set Layer2
-    ACTION_LAYER_TOGGLE(3),                         // FN3 - toggle Layer3 aka Numpad layer
-    ACTION_LAYER_SET(0, ON_PRESS),                  // FN4 - set Layer0
+    ACTION_LAYER_MOMENTARY(2),                      // FN2 - switch to Layer2
+    ACTION_LAYER_MOMENTARY(3),                      // FN3 - switch to Layer2
+    ACTION_MODS_KEY(MOD_LSFT, KC_GRAVE),            // FN4 - Tilde
+    ACTION_MODS_KEY(MOD_LSFT, KC_LBRACKET),         // FN5 - {
+    ACTION_MODS_KEY(MOD_LSFT, KC_RBRACKET),         // FN6 - }
+    ACTION_MODS_KEY(MOD_LSFT, KC_6),                // FN7 - ^
+    ACTION_MODS_KEY(MOD_LSFT, KC_4),                // FN8 - $
+    ACTION_MODS_KEY(MOD_LSFT, KC_7),                // FN9 - &
+    ACTION_MODS_KEY(MOD_LSFT, KC_9),                // FN10 - (
+    ACTION_MODS_KEY(MOD_LSFT, KC_0),                // FN11 - )
+    ACTION_MODS_KEY(MOD_LSFT, KC_SCLN),             // FN12 - ;
+    ACTION_MODS_KEY(MOD_LSFT, KC_2),                // FN13 - @
+    ACTION_MODS_KEY(MOD_LSFT, KC_SLSH),             // FN14 - ?
+    ACTION_MODS_KEY(MOD_LSFT, KC_3),                // FN15 - #
+    ACTION_MODS_KEY(MOD_LSFT, KC_5),                // FN16 - %
+    ACTION_MODS_KEY(MOD_LSFT, KC_BSLS),             // FN17 - |
+    ACTION_MODS_KEY(MOD_LSFT, KC_8),                // FN18 - *
+    ACTION_MODS_KEY(MOD_LSFT, KC_1),                // FN19 - !
+
 };
 
 void action_function(keyrecord_t *event, uint8_t id, uint8_t opt)
